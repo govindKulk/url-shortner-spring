d6ad3f00e1b61c9591a2f8b03c99a00d6f60851a
govindKulk <kulkarnigovind2003@gmail.com>
2025-06-23 19:11:47 +0530

    genrated api-gateway mservice. fixed cloud-gatway not working with starter-web error

--- service-discovery/src/main/java/com/govindkulk/service_discovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/govindkulk/service_discovery/ServiceDiscoveryApplication.java
@@ -2,8 +2,11 @@ package com.govindkulk.service_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
